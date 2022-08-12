fc4f3703f1e584451893a5f7220cc39326a81ac9
shaunoommen.oa <shaunoommen.alexander2018@vitstudent.ac.in>
2022-08-12 14:38:21 +0530

    change to get using parameter url passing

--- src/main/java/com/example/controller/CustomerRequestController.java
+++ src/main/java/com/example/controller/CustomerRequestController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,11 +23,10 @@ public class CustomerRequestController {
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value="/getEmpInfo", method=RequestMethod.GET)
-	public Object getEmpDetails(@RequestBody Map<String, String> userMap) {
+	public Object getEmpDetails(@PathVariable String type, @PathVariable String status) {
 		
-		System.out.println(userMap.toString());
-		
-		return(requestService.getRequestByType(userMap.get("type"), userMap.get("status")));
+		System.out.println(type+"---"+status);
+		return(requestService.getRequestByType(type, status));
 		
 	}
 	
